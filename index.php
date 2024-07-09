af8173e2d7dcca7024877914a3c1efc8f261f701
KaremED <kareemedres1@gmail.com>
2024-07-09 19:32:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Simple Login</title>
+    </head>
+<body>
+
+<form action="chk.php" method="POST">
+
+    <input type="text" name="username">
+    <br>
+    <input type="password" name="password">
+    <input type="submit" name="Login">
+
+    <?php echo "<br>passwor hint 123"; ?>
+
+</form>
+
+</body>
+
+</html>
